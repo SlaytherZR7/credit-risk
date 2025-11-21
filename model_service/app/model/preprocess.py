8f95d72bc9b7cd6789034a44beca6b9fa8e88670
Slayther Zamora <slayther.zr@gmail.com>
2025-11-20 22:13:59 -0500

    Remove unused preprocessing and utility files related to QuantileDiscretizer

--- model_service/app/model/preprocess.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-BASE_DIR = "/app/artifacts"
-EXPECTED_COLS_PATH = os.path.join(BASE_DIR, "expected_columns.json")
-
-with open(EXPECTED_COLS_PATH) as f:
-    EXPECTED_COLS = list(json.load(f).keys())
